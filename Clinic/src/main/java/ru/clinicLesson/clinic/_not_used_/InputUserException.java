1e3cc0496c51522c0422ba62fbfccff3355905d6
Panchenko Vlad <p.vlad96@gmail.com>
2016-04-09 22:55:10 +0300

    lessons

--- Clinic/src/main/java/ru/clinicLesson/clinic/_not_used_/InputUserException.java
+++
@@ -1,8 +0,0 @@
-package ru.clinicLesson.clinic._not_used_;
-
-public class InputUserException extends Exception {
-
-    public InputUserException(final String message) {
-        super(message);
-    }
-}
