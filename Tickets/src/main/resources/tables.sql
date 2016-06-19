a2f2bebd92463f832accbdb829331c9361a15cce
Panchenko Vlad <p.vlad96@gmail.com>
2016-06-20 00:16:35 +0300

    clear

---
+++ Tickets/src/main/resources/tables.sql
@@ -0,0 +1,14 @@
+﻿create table tickets (
+id serial primary key,
+country varchar(40),
+price int
+);
+
+create table reservations (
+id serial primary key,
+name varchar(20),
+surname varchar(25),
+telephone bigint,
+ticket_id int,
+foreign key (ticket_id) references tickets(id)
+);
