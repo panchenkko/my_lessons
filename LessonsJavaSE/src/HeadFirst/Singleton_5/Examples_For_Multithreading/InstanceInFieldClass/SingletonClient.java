d1db982882e8a37dc60bebe27f5a0c9d4ecd37a5
Panchenko Vlad <p.vlad96@gmail.com>
2016-07-15 15:00:45 +0300

    Шаблон проектирования "Одиночка"

---
+++ LessonsJavaSE/src/HeadFirst/Singleton_5/Examples_For_Multithreading/InstanceInFieldClass/SingletonClient.java
@@ -0,0 +1,7 @@
+package HeadFirst.Singleton_5.Examples_For_Multithreading.InstanceInFieldClass;
+
+public class SingletonClient {
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+	}
+}
