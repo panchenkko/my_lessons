1d111fadb7ccf736ac6860d5730cff33e4e59e34
Panchenko Vlad <p.vlad96@gmail.com>
2016-10-09 16:29:33 +0300

    Потоки ввода/вывода

--- LessonsJavaSE/src/LessonsJavaCore/Other/Multithreading/CourseGolovach/v6/MyExample7/MyExample.java
+++
@@ -1,7 +0,0 @@
-package LessonsJavaCore.Other.Multithreading.CourseGolovach.v6.MyExample7;
-
-public class MyExample {
-    public static void main(String[] args) {
-        new Index().run();
-    }
-}
