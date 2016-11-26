f9f3645017bfd3d802504501406abdd4bc7b26af
Panchenko Vlad <p.vlad96@gmail.com>
2016-11-26 21:28:07 +0200

    Добавлены небольшие правки в нескольких проектах

--- ClinicPetWeb/src/main/java/ru/clinicPetWeb/store/JdbcStorage.java
+++ ClinicPetWeb/src/main/java/ru/clinicPetWeb/store/JdbcStorage.java
@@ -9,7 +9,6 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 
 @Repository
 public class JdbcStorage implements Storage {
